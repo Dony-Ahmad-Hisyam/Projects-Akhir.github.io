0e0cf09b0fe3f0f2a5c6c29f5c13077756fc65fe
alifaditya0 <danigilass@yahoo.com>
2022-11-16 00:01:41 +0700

    alifbeknaa

--- src/commponents/content2.jsx
+++ src/commponents/content2.jsx
@@ -1,14 +1,221 @@
+import { Col, Container, Row, Card } from "react-bootstrap";
 import "../App";
 import Bg from "../asset/bg/bg6.jpg";
+import Bg5 from "../asset/bg/bg5.jpg";
+import Aum from "../asset/img/aum.jpg";
+import Black from "../asset/img/blackadam.jpg";
+import BlackP from "../asset/img/blackpanther.jpg";
+import Whole from "../asset/img/whole.jpg";
+import Kkn from "../asset/img/kkn.jpg";
+import Perempuan from "../asset/img/perempuan.jpg";
+import Dora from "../asset/img/dora.png";
+import Century from "../asset/img/centurygirl.jpeg";
+import Crawl from "../asset/img/crawl.jpeg";
+import Raya from "../asset/img/raya.jpg";
+import Silenced from "../asset/img/silenced.jpg";
+import One from "../asset/img/one.jpeg";
 
-const Content = () => {
+
+const Content2 = () => {
   return (
     <>
       <img className="UkuranBg" src={Bg} alt="" />
-  
+      <img src={Bg5} alt="" />
+  <div className="Content3">
+    <Container>
+      <Row>
+       
+      
+        
+       
+      
+      
+        <Col className="Picture">
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={BlackP} />
+      <div className="bg-dark">
+      <Card.Body>
+<div className="p-2 m-1 text-white">
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        </div>
+      </Card.Body>
+        </div>
+
+    </Card>
+        </Col>
+      
+      
+        <Col className="Picture">
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={Whole} />
+      <div className="bg-dark">
+      <Card.Body>
+<div className="p-2 m-1 text-white">
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </div>
+      </Card.Body>
+      </div>
+
+    </Card>
+        </Col>
+      
+      
+        <Col className="Picture">
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={Kkn} />
+      <div className="bg-dark">
+      <Card.Body>
+<div className="p-2 m-1 text-white">
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </div>
+      </Card.Body>
+</div>
+    </Card>
+        </Col>
+      
+      
+        <Col className="Picture">
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={Perempuan} />
+      <div className="bg-dark">
+      <Card.Body>
+<div className="p-2 m-1 text-white">
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </div>
+      </Card.Body>
+</div>
+    </Card>
+        </Col>
+      
+      
+        <Col className="Picture">
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={Dora} />
+      <div className="bg-dark">
+      <Card.Body>
+<div className="p-2 m-1 text-white">
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </div>  
+      </Card.Body>
+</div>
+    </Card>
+        </Col>
+      
+      
+        <Col className="Picture">
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={Century} />
+      <div className="bg-dark">
+      <Card.Body>
+<div className="p-2 m-1 text-white">
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        </div>
+      </Card.Body>
+</div>
+    </Card>
+        </Col>
+      
+      
+        <Col className="Picture">
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={Crawl} />
+      <div className="bg-dark">
+      <Card.Body>
+<div className="p-2 m-1 text-white">
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        </div>
+      </Card.Body>
+</div>
+    </Card>
+        </Col>
+      
+      
+        <Col className="Picture">
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={Raya} />
+      <div className="bg-dark">
+      <Card.Body>
+<div className="p-2 m-1 text-white">
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        </div>
+      </Card.Body>
+</div>
+    </Card>
+        </Col>
+      
+      
+        <Col className="Picture">
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={Silenced} />
+      <div className="bg-dark">
+      <Card.Body>
+<div className="p-2 m-1 text-white">
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        </div>
+      </Card.Body>
+</div>
+    </Card>
+        </Col>
+      
+      
+        <Col className="Picture">
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={One} />
+      <div className="bg-dark">
+      <Card.Body>
+<div className="p-2 m-1 text-white">
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </div>
+      </Card.Body>
+</div>
+    </Card>
+        </Col>
+        </Row>
+    </Container>
+  </div>
 
     </>
   );
 };
 
-export default Content;
+export default Content2;
